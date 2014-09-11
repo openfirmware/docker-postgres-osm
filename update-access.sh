94c0f97567940be46643f354e4d7870781bd9c6b
James Badger <jamesbadger@gmail.com>
2014-09-10 23:36:17 -0600

    Add shebang and error check to access script

--- update-access.sh
+++ update-access.sh
@@ -1,5 +1,7 @@
 # Modify auth file to allow all from any host. This is okay with a
 # container that doesn't publish its ports.
+#!/bin/bash
+set -e
 
 { echo; echo 'host all all 0.0.0.0/0 trust'; } >> "$PGDATA"/pg_hba.conf
 
